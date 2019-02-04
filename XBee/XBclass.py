d38df4c3a5a9ea4438efacf32ea6392fed7c9e66
Visa <visa.tuominen@iki.fi>
2019-02-04 20:08:30 +0200

    class for "master" local device as well as individual sensors

--- XBee/XBclass.py
+++ XBee/XBclass.py
@@ -1,24 +1,22 @@
 from digi.xbee.io import IOLine, IOMode, IOValue
+from digi.xbee.devices import XBeeDevice
+# omat
 from XBdevices import deviceTypes
+import time
 
 
 # Hypotethical class for a single sensor?
-class XBeePin:
-    """
-    Class for single xbee sensor:
-        device = sensor's xbee device
-        dvcType = device type
-        pin = pin where sensor is connected.
-    """
-    def __init__(self, device, dvcType, pin):
-        self.dvcType = dvcType
-        self.xbee = device
+class XBeeSensor:
+    "class for a single xbee pin/sensor. Has functions for reading and writing values!"
+    def __init__(self, pin, xbee):
+        self.xbee = xbee
+        self.dvcType = xbee.get_node_id()
         self.ioPin = pin
+        print("Setting pin " + str(pin) + " of " + self.dvcType, end="")
         self.pinLine = deviceTypes[self.dvcType][self.ioPin]["line"]
         self.pinMode = deviceTypes[self.dvcType][self.ioPin]["mode"]
         self.xbee.set_io_configuration(self.pinLine,self.pinMode)
-
-
+        print(" ...OK!")
     def get_raw_value(self):
         value = None
         if self.pinMode == IOMode.ADC:
@@ -29,20 +27,60 @@ class XBeePin:
             print("invalid pin mode!")
         return value
     
-    def write_pin(self, value):
+    def write_dio(self, value):
+        "function to write digital io values to pin"
+        if self.pinMode == IOMode.DIGITAL_OUT_HIGH or self.pinMode == IOMode.DIGITAL_OUT_LOW:
+            self.xbee.set_dio_value(self.pinLine, value)
+        else:
+            print("invalid pin mode!")
+
+    def write_pwm(self, value):
+        "function to write pwm value to pin"
         if self.pinMode == IOMode.PWM:
             if (0 < int(value) < 100):
                 self.xbee.set_pwm_duty_cycle(self.pinLine, value)
             else:
                 print("invalid value!")
-        elif self.pinMode == IOMode.DIGITAL_OUT_HIGH or self.pinMode == IOMode.DIGITAL_OUT_LOW:
-            if value == 0:
-                outValue = IOValue.LOW
-            elif value == 1:
-                outValue = IOValue.HIGH
-            else:
-                print("Invalid value!")
-            self.xbee.set_dio_value(self.pinLine, outValue)
         else:
             print("invalid pin mode!")
 
+
+class masterXBee:
+    '''
+    Class for a local XBee device
+    port = used COM/tty port
+    baud = used baud rate
+    deviceType = chosen device type from XBdevices.deviceTypes
+    sensors = dict of all the devices in locals network and their sensors!
+    '''
+    sensors = {}
+    def __init__(self, port, baud):
+        print("Opening local device", port, baud)
+        self.xb = XBeeDevice(port,baud)
+        self.xb.open()
+        self.deviceType = self.xb.get_node_id()
+        self.remotes = self.getNetworkDevices()
+        for device in self.remotes:
+            deviceType = device.get_node_id()
+            self.sensors.update({deviceType:{}})
+            for pin in deviceTypes[deviceType]:
+                sensor = XBeeSensor(pin, device)
+                self.sensors[deviceType][pin] = sensor
+    def getNetworkDevices(self):
+        """
+        Returns devices in the network of given local xBee device, including the local device
+        type is RemoteXbeeDevice
+        """
+        print("Getting a list of network devices...")
+        # Get devices in local xbee's network!
+        self.xb.send_data_broadcast("Getting a list of everyone :D")
+        network = self.xb.get_network()
+        network.start_discovery_process()
+        while network.is_discovery_running():
+            time.sleep(.5)
+        networkDevices = network.get_devices()
+        print("Found the following devices:")
+        for found in networkDevices:
+            print(found)
+        networkDevices.insert(0, self.xb)
+        return networkDevices
